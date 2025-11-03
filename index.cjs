b7a681373edb91ce1cdfb80a8db5af5e6903ffce
Lucas <lvazgar131@g.educaand.es>
2025-11-03 14:28:35 +0100

    Primer commit del proyecto colaborativo. Empezamos con el script principal del backend

---
+++ index.cjs
@@ -0,0 +1,21 @@
+const conector = require("./database/conexion.cjs");
+const express = require("express");
+const cors = require("cors");
+const persona_paths = require("./paths/persona.cjs");
+const cookieParser = require("cookie-parser");
+const fs = require("fs");
+console.log("Iniciamos");
+require("dotenv").config();
+conector.conexion();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.text({ type: "text/plain" }));
+app.use(persona_paths.router);
+app.use(express.static("public"));
+const PORT = 5050;
+app.listen(PORT, "0.0.0.0", () => {
+console.log(`Servidor HTTP corriendo en el puerto ${PORT}`);
+});
